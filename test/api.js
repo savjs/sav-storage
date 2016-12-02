cdc7c4c6c91e2498fed0cbe2df3aaa3752a9ecad
hsd <51134642@qq.com>
2016-12-02 20:07:05 +0800

    添加验证Object类型

--- test/api.js
+++ test/api.js
@@ -1,10 +1,11 @@
 import test from 'ava'
 import {Storage} from '../'
-import {isFunction} from 'sav-util'
+import {isFunction, isObject} from 'sav-util'
 
 test('Storage#storage', ava => {
   ava.true(isFunction(Storage))
   let storage = new Storage()
+  ava.true(isObject(storage))
   ava.true(isFunction(storage.use))
   ava.true(isFunction(storage.provider))
   ava.true(isFunction(storage.get))
